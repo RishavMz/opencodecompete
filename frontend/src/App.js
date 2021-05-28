507fbc847a2c3f1b3fbaab12561b4a7fedf92f10
Rishav Mazumdar <rishav.mazumdar123@gmail.com>
2021-05-28 12:59:36 +0530

    Redirecting respect to login cookie

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,21 @@
 import Main from './components/main/main'
 import Auth from './components/auth/auth'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 function App() {
+
+  const loginCookie = document.cookie.substring(document.cookie.indexOf("login=200")+9);
+
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route path = "/" exact component = {Main}/>
-          <Route path = "/login" exact component = {Auth}/>
+          <Route path = "/" exact component = {Main}>
+            {loginCookie === ""? <Redirect to = "/login"/> : null}
+          </Route>
+          <Route path = "/login" exact component = {Auth}>
+          {loginCookie !== ""? <Redirect to = "/"/> : null}
+          </Route>
         </Switch>
       </Router>
     </div>
