ffcb4aba38484ea139537483b03c5e72cc2bdbd4
Rishav Mazumdar <rishav.mazumdar123@gmail.com>
2021-07-18 11:26:41 +0530

    Fixing that mysterious bug

--- backend/dbconn.js
+++ backend/dbconn.js
@@ -1,6 +1,8 @@
 const {Pool} = require("pg");
 require("dotenv").config();
 
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+
 const conn = new Pool({
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
